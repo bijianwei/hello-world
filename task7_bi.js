79e5649b2a7f3d8b8a2f81dd0737c3369a29235e
bijianwei <316215949@qq.com>
2017-08-30 11:12:49 +0800

    Create task7_bi.js

---
+++ task7_bi.js
@@ -0,0 +1,20 @@
+window.onload = function(){
+  var oTop = document.getElementById("uptotop");
+  var screenw = document.documentElement.clientWidth || document.body.clientWidth;
+  var screenh = document.documentElement.clientHeight || document.body.clientHeight;
+  oTop.style.left = screenw - oTop.offsetWidth- 40 +"px";
+  oTop.style.top = screenh - oTop.offsetHeight- 20 + "px";
+  window.onscroll = function(){
+    var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+    oTop.style.top = screenh - oTop.offsetHeight-150 + scrolltop +"px";
+  }
+  oTop.onclick = function(){
+    document.documentElement.scrollTop = document.body.scrollTop =0;
+  }
+  oTop.onmouseover = function(){
+	  oTop.style.backgroundColor = "red";
+  }
+  oTop.onmouseout = function(){
+	  oTop.style.backgroundColor = "gray";
+  }
+}  
